a809bc57ca1dfc7928ef435e7e3d0a71c42dcfdf
KhanAhzam <ahzam003@gmail.com>
2025-04-12 15:57:54 +0530

    Landing Page without final images and icons

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,6 +6,7 @@ const LandingPage = () => {
   const navigate = useNavigate();
   return (
     <>
+
       <div className="navbar bg-[#EBE7DE]">
 
         <div className='container mx-auto'>
@@ -68,10 +69,101 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="container mx-auto recommend ml-10 mt-8">
+      <div className="recommend container mx-auto mt-4">
         <div className="title text-[#CCAB4A] font-bold text-3xl underline underline-offset-2 decoration-2">Recommended Vendors</div>
-        div.
+        <div className="vendor_images mt-4 flex gap-10">
+          <div className="first">
+            <div className="catering mb-5 w-[360px] h-[240px] bg-gray-500 rounded-[18px]"></div>
+            <div className="entertainment w-[360px] h-[240px] bg-gray-500 rounded-[18px]"></div>
+          </div>
+          <div className="second decorator w-[350px] h-[500px] bg-gray-500 rounded-[18px]"></div>
+          <div className="third">
+            <div className="photography mb-5 w-[360px] h-[240px] bg-gray-500 rounded-[18px]"></div>
+            <div className="gift w-[360px] h-[240px] bg-gray-500 rounded-[18px]"></div>
+          </div>
+          <div className="fourth moretocome w-[350px] h-[500px] bg-gray-500 rounded-[18px]"></div>
+        </div>
       </div>
+
+      <div className="themed container mx-auto mt-4">
+        <div className="title text-[#CCAB4A] font-bold text-3xl underline underline-offset-2 decoration-2">Get Themed Decorations</div>
+        <div className="cards flex mt-4 gap-10 justify-center">
+          <div className="first_card w-[470px] h-[342px] rounded-2xl bg-gray-200 flex flex-col items-center">
+            <div className="img w-[435px] h-[240px] rounded-2xl bg-purple-300 mt-4">
+              {/* Image to be place here */}
+            </div>
+            <div className="textbelow mt-3.5 flex flex-col justify-center items-center">
+              <div className="theme font-semibold text-xl">Halloween Theme</div>
+              <div className="pricing text-[#CCAB4A]">Contact for Pricing</div>
+            </div>
+          </div>
+          <div className="second_card w-[470px] h-[342px] rounded-2xl bg-gray-200 flex flex-col items-center">
+            <div className="img w-[435px] h-[240px] rounded-2xl bg-purple-300 mt-4">
+              {/* Image to be place here */}
+            </div>
+            <div className="textbelow mt-3.5 flex flex-col justify-center items-center">
+              <div className="theme font-semibold text-xl">Kids Theme</div>
+              <div className="pricing text-[#CCAB4A]">Contact for Pricing</div>
+            </div>
+          </div>
+          <div className="third_card w-[470px] h-[342px] rounded-2xl bg-gray-200 flex flex-col items-center">
+            <div className="img w-[435px] h-[240px] rounded-2xl bg-purple-300 mt-4">
+              {/* Image to be place here */}
+            </div>
+            <div className="textbelow mt-3.5 flex flex-col justify-center items-center">
+              <div className="theme font-semibold text-xl">Christmas Theme</div>
+              <div className="pricing text-[#CCAB4A]">Contact for Pricing</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="vendornetwrok container mx-auto mt-10 mb-10 w-[1600px] h-[130px] bg-[#EBE7DE] rounded-2xl flex justify-center items-center">
+        <div className="contains w-full pl-10 pr-10 flex justify-between items-center">
+          <div className="left">
+            <div className="icon">
+              {/* Icon to be placed here */}
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-2xl font-bold'>Exclusive deals for our Vendor partners !</span>
+              <span className='text-[#CCAB4A] text-lg'>Looking for exclusive deals on our services? Sign Up to access special offers from our us</span>
+            </div>
+          </div>
+          <div className="right">
+            <button type="button" className='bg-[#CCAB4A] w-[270px] rounded-full p-3 flex items-center justify-center text-white font-bold'>
+                <span className='text-xl font-semibold'>JOIN VENDOR NETWORK</span>
+                {/* Onclick to be applied on above */}
+            </button>
+        </div>
+        </div>
+      
+      </div>      
+
+      <div className="vendornetwrok container mx-auto mt-10 mb-10 w-[1600px] h-[190px] bg-[#EBE7DE] rounded-2xl">
+
+        <div className="containsfooter w-full h-full flex justify-between items-center pl-10 pr-10">
+
+          <div className="left flex flex-col justify-between pt-5 pb-5 h-full">
+            <div className="top flex flex-col">
+              <span className='font-bold text-3xl'>tendr</span>
+              <span className='text-[#CCAB4A] font-medium'>Simplified event planning</span>
+            </div>
+            <div className="bottom">
+              <div className='font-semibold text-lg'>tendr ©</div>
+            </div>
+          </div>
+
+          <div className="right pt-5 pb-5 h-full flex flex-col justify-evenly">
+            <span className='font-medium'>Support</span>
+            <span className='font-medium'>Help Center</span>
+            <span className='font-medium'>Vendor Support</span>
+            <span className='font-medium'>Vendor</span>
+            <span className='font-medium'>Get in touch</span>
+          </div>
+
+        </div>
+
+      </div>      
 
     </>
   )
